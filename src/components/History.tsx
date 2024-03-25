ce533baadc18e422f2b0271763fda96116ce68b4
Ruihang Xia <waynestxia@gmail.com>
2024-03-25 23:10:53 +0800

    fix: reorganize history buttons
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -12,28 +12,38 @@ class ExecuteHistory {
 export const historyListAtom = atom([initialGreeting])
 
 const actions = [
-  { tip: "Copy Result", icon: "i-tabler-copy", action: (executeHistory: ExecuteHistory) => { navigator.clipboard.writeText(executeHistory.result) } },
+  { tip: "Delete", icon: "i-tabler-trash", action: () => { } },
+  { tip: "Collapse", icon: "i-tabler-box-align-top", action: () => { } },
   { tip: "Copy Query", icon: "i-tabler-arrow-left-to-arc", action: (executeHistory: ExecuteHistory) => { navigator.clipboard.writeText(executeHistory.query) } },
+  { tip: "Copy Result", icon: "i-tabler-copy", action: (executeHistory: ExecuteHistory) => { navigator.clipboard.writeText(executeHistory.result) } },
 ]
+
+function ActionPanel(executeHistory: ExecuteHistory) {
+  return (
+    <div className="hidden group-hover-display-unset">
+      {
+        actions.map((action, index) => (
+          <Tooltip key={index} label={action.tip}>
+            <ActionIcon
+              className={"m-r-2"}
+              variant="outline"
+              color="gray"
+              onClick={() => { action.action(executeHistory) }}>
+              <div className={action.icon} />
+            </ActionIcon>
+          </Tooltip>)
+        )
+      }
+    </div>
+  )
+}
 
 function HistoryItem(executeHistory: ExecuteHistory) {
   return (
     <div className={executeHistory.isErr ? "bg-red-200" : ""}>
       <div className="py-1 m1 position-relative group border-solid border-1 border-rd border-transparent hover:border-gray">
-        <div className="hidden group-hover-display-unset">
-          {
-            actions.map((action, index) => (
-              <Tooltip key={index} label={action.tip}>
-                <ActionIcon
-                  className={"position-absolute top-4 right-" + (index * 8 + 4)}
-                  variant="outline"
-                  color="gray"
-                  onClick={() => { action.action(executeHistory) }}>
-                  <div className={action.icon} />
-                </ActionIcon>
-              </Tooltip>)
-            )
-          }
+        <div className="hidden group-hover-display-unset position-absolute right-0 top-2">
+          <ActionPanel {...executeHistory} />
         </div>
 
         <div className="break-words whitespace-pre-line">
